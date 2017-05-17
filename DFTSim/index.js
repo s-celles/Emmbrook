55c3e4070ae8556b83941b8beaa264d47caed8ee
Lenoque <singularitti@outlook.com>
2017-05-17 00:02:33 -0400

    Renew file structure

---
+++ DFTSim/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Qi on 5/16/17.
+ */
+
+// $(function () {
+//     $('[data-toggle="tooltip"]').tooltip({
+//         effect: "blind",
+//         delay: {"show": 0, "hide": 100},
+//         show: null
+//     });
+// });
+
+$(document).tooltip({show: null});
--- tabs.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Qi on 5/16/17.
- */
-
-$(function () {
-    $("#tabs").tabs();
-});
