e56066f4f4ab15c9942f39654bd9aa80fe550675
Lenoque <singularitti@outlook.com>
2017-05-25 17:21:45 -0400

    Finish modal in index.html

--- DFTSim/assets/js/index.js
+++ DFTSim/assets/js/index.js
@@ -3,3 +3,11 @@
  */
 
 $(document).tooltip({show: null});
+
+$(document).ready(function () {
+        $("#pop").on("click", function () {
+            $("#img-preview").attr("src", $("#prime-img").attr("src"));
+            $("#img-modal").modal("show");
+        })
+    }
+);
