92a010554dd10bdd02be78c360e0413404205ab1
Lenoque <singularitti@outlook.com>
2017-05-25 14:48:09 -0400

    Add thumbnail borders to index.html

--- DFTSim/assets/js/index.js
+++ DFTSim/assets/js/index.js
@@ -2,12 +2,4 @@
  * Created by Qi on 5/20/17.
  */
 
-// $(function () {
-//     $('[data-toggle="tooltip"]').tooltip({
-//         effect: "blind",
-//         delay: {"show": 0, "hide": 100},
-//         show: null
-//     });
-// });
-
 $(document).tooltip({show: null});
