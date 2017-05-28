39dada46b2c3b006b5940fd7577fffb1e9709772
Lenoque <singularitti@outlook.com>
2017-05-28 15:06:58 -0400

    Debug newpanel5.js

---
+++ DFTSim/assets/js/panel5.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Qi on 5/28/17.
+ */
+
+var sampleSlider = $('mySamples').bootstrapSlider();
+var phaseSlider = $('myPhase').bootstrapSlider();
+var numSamples = sampleSlider.bootstrapSlider('getValue');
+var phase = phaseSlider.bootstrapSlider('getValue');
