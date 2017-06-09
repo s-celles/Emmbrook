37a970a70046197c8b9b85b38063ffa6e4f4fd33
Sucan <qi.zhang@columbia.edu>
2017-06-08 20:42:16 -0400

    Add new way

--- EM/assets/js/oblique.js
+++ EM/assets/js/oblique.js
@@ -9,55 +9,80 @@ var thetaISlider = $('#thetaI').bootstrapSlider();
 var n1 = n1Slider.bootstrapSlider('getValue');  // Get refraction index from slider bar
 var n2 = n2Slider.bootstrapSlider('getValue');  // Get refraction index from slider bar
 var thetaI = thetaISlider.bootstrapSlider('getValue');
+var plt = document.getElementById('plt');
 
 var epsilon1 = Math.pow(n1, 2);  // Permittivity
 var epsilon2 = Math.pow(n2, 2);  // Permittivity
-var alpha, beta, t, r, thetaIList;
-var plt = document.getElementById('plt');
+var nx = 200;  // Grid size
+var nz = 200;  // Grid size
 
 // Basic interfaces
-function updateValues(thetaI) {
-    alpha = Math.sqrt(1 - Math.pow(n1 / n2, 2) * Math.pow(Math.sin(thetaI), 2)) / Math.cos(thetaI);
-    beta = n1 / n2 * epsilon2 / epsilon1;
-    t = 2 / (alpha + beta);  // Transmission amplitude
-    r = (alpha - beta) / (alpha + beta);  // Reflection amplitude
+function sqr(x) {
+    return Math.pow(x, 2)
 }
 
-function updateHeatmapValues() {
-    thetaIList = numeric.linspace(0, Math.PI / 2, 500);
-    var alpList = thetaIList.map(function (thetaI) {
-        return Math.sqrt(1 - Math.pow(n1 / n2, 2) * Math.pow(Math.sin(thetaI), 2)) / Math.cos(thetaI)
-    });
-    beta = n1 / n2 * epsilon2 / epsilon1;
-    t = alpList.map(function (alp) {
-        return 2 / (alp + beta)
-    });
-    r = alpList.map(function (alp) {
-        return (alp - beta) / (alp + beta)
-    })
+function dist2(v, w) {
+    /*
+     Calculate 2D distance between 2 points v and w.
+     */
+    return Math.sqrt(sqr(v.x - w.x) + sqr(v.z - w.z))
 }
 
-// Plot
-function plot() {
-    plt.data[0].y = r;
-    plt.data[1].y = t;
-    Plotly.redraw(plt)
+function dist2PointToSegment(u, v, w) {
+    /*
+     Calculate 2D distance between a point u and a line segment vw.
+     */
+    var lwv = dist2(w, v);
+    var luv = dist2(u, v);
+    if (lwv === 0) {
+        return luv
+    }
+
+    var cosTheta = ((u.x - v.x) * (w.x - v.x) + (u.z - v.z) * (w.z - v.z)) / (lwv * luv);  // Cosine of angle between 2 segments
+    var sinTheta = Math.sqrt(1 - sqr(cosTheta));  // Sine of angle between 2 segments
+    return luv * sinTheta
 }
 
-function createHeatmapPlot() {
-    var layout = {
-        yaxis: {title: 'Ratio', titlefont: {size: 18}},
-        xaxis: {title: "\\( \\theta_I \\)", titlefont: {size: 18}}
-    };
+function generate2DGrid(nx, nz) {
+    var grid = [];
 
-    var data = [
-        {x: thetaIList, y: r, type: 'scatter', mode: 'lines', name: 'r'},
-        {x: thetaIList, y: t, type: 'scatter', mode: 'lines', name: 't'}
-    ];
-
-    Plotly.newPlot(plt, data, layout)
+    for (var i = 0; i < nx; i++) {
+        grid[i] = [];
+        for (var j = 0; j < nz; j++) {
+            grid[i][j] = {
+                x: i / nx,  // Normalize grid coordinate
+                z: j / nz,  // Normalize grid coordinate
+                eFieldIntens: 0
+            };
+        }
+    }
+    return grid
 }
 
+function generateDecayFactor(u, v, w) {
+    /*
+     The electric field intensity I decays as exp(-r^2), where r is the radial distance of point u from line vw.
+     */
+    var r = dist2PointToSegment(u, v, w);
+    return Math.exp(-sqr(r));
+}
+
+function assignDecayFactor(grid, v, w) {
+    /*
+     Assign each grid point an electric field intensity, according to its distance from line vw.
+     */
+    for (var i = 0; i < nx; i++) {
+        for (var j = 0; j < nz; j++) {
+            var u = grid[i][j];
+            var d = generateDecayFactor(u, v, w);
+            u.eFieldIntens = 1 * d;  // The intensity on line segment vw is 1
+        }
+    }
+    return grid
+}
+
+var grid2D = generate2DGrid(nx, nz);
+console.log(assignDecayFactor(grid2D, grid2D[0][0], grid2D[100][100]));
 
 // Interactive interfaces
 thetaISlider.on('slideStop', function () {
@@ -83,5 +108,3 @@ n2Slider.on('slideStop', function () {
 });
 
 // Initialize
-updateHeatmapValues();
-createHeatmapPlot();
