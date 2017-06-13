e04dbe21ce3a6874aad62486c05d07288a9256e2
Sucan <qi.zhang@columbia.edu>
2017-06-13 01:29:45 -0400

    Add something

--- EM/assets/js/oblique.rt.js
+++ EM/assets/js/oblique.rt.js
@@ -4,82 +4,71 @@
 
 var thetaIList = numeric.linspace(0, Math.PI / 2, 200);
 
-function updateRatioValues(tI) {
-  /*
-   Accept an incident angle tI, return the reflection ratio and transmission ratio.
-   */
-  var alpha = Math.sqrt(1 - Math.pow(n1 / n2, 2) * Math.pow(Math.sin(tI), 2)) / Math.cos(tI);
-  var beta = n1 / n2 * epsilon2 / epsilon1;
-  var t = 2 / (alpha + beta);
-  var r = (alpha - beta) / (alpha + beta);
-  return [r, t]
-}
-
 function updateRatioLists() {
-  /*
-   Return: An array of [[r0, r1, ...], [t0, t1, ...]].
-   */
-  return numeric.transpose(thetaIList.map(updateRatioValues))
+    /*
+     Return: An array of [[r0, r1, ...], [t0, t1, ...]].
+     */
+    return numeric.transpose(thetaIList.map(updateRatioValues))  // Defined in oblique.main.js
 }
 
 // Plot
 function plotRatios() {
-  plt1.data[2].x = [thetaI, thetaI];
-  plt1.data[2].y = updateRatioValues(thetaI);
-  Plotly.redraw(plt1)
+    plt1.data[2].x = [thetaI, thetaI];
+    plt1.data[2].y = updateRatioValues(thetaI);
+    Plotly.redraw(plt1)
 }
 
 function plotRatioLists() {
-  plt1.data[0].y = reflectRatioList;
+    plt1.data[0].y = reflectRatioList;
 
-  plt1.data[1].y = transmitRatioList;
-  Plotly.redraw(plt1)
+    plt1.data[1].y = transmitRatioList;
+    Plotly.redraw(plt1)
 }
 
 function createRatioPlot() {
-  var layout = {
-    title: 'reflection and transmission ratio',
-    xaxis: {
-      title: 'theta_I',
-      titlefont: {
-        size: 18
-      },
-      tickmode: 'array',
-      tickvals: [0, Math.PI / 12, Math.PI / 6, Math.PI / 4, Math.PI / 3, Math.PI / 2],
-      ticktext: ['0', 'pi/12', 'pi/6', 'pi/4', 'pi/3', 'pi/2']
-    },
-    yaxis: {
-      title: 'Ratio',
-      titlefont: {
-        size: 18
-      }
-    }
-  };
+    var layout = {
+        title: 'reflection and transmission ratio',
+        xaxis: {
+            title: 'theta_I',
+            titlefont: {
+                size: 18
+            },
+            tickmode: 'array',
+            tickvals: [0, Math.PI / 12, Math.PI / 6, Math.PI / 4, Math.PI / 3, Math.PI / 2],
+            ticktext: ['0', 'pi/12', 'pi/6', 'pi/4', 'pi/3', 'pi/2']
+        },
+        yaxis: {
+            title: 'Ratio',
+            titlefont: {
+                size: 18
+            }
+        }
+    };
 
-  var data = [{
-      x: thetaIList,
-      y: reflectRatioList,
-      type: 'scatter',
-      mode: 'lines',
-      name: 'r'
+    var data = [{
+        x: thetaIList,
+        y: reflectRatioList,
+        type: 'scatter',
+        mode: 'lines',
+        name: 'r'
     },
-    {
-      x: thetaIList,
-      y: transmitRatioList,
-      type: 'scatter',
-      mode: 'lines',
-      name: 't'
-    },
-    {
-      x: [thetaI, thetaI],
-      y: updateRatioValues(thetaI),
-      typr: 'scatter',
-      mode: 'markers',
-      name: 'ratios'
-    }
-  ];
+        {
+            x: thetaIList,
+            y: transmitRatioList,
+            type: 'scatter',
+            mode: 'lines',
+            name: 't'
+        },
+        {
+            x: [thetaI, thetaI],
+            y: updateRatioValues(thetaI),
+            typr: 'scatter',
+            mode: 'markers',
+            name: 'ratios'
+        }
+    ];
 
-  Plotly.newPlot(plt1, data, layout)
+    Plotly.newPlot(plt1, data, layout)
 }
 
 // Initialize
