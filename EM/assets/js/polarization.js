2edf058a5860aeb4c53034cd522c35c7d3d5d64a
Sucan <qz2280@columbia.edu>
2017-07-19 23:10:35 -0400

    Slightly improve polarization extensibility

--- EM/assets/js/polarization.js
+++ EM/assets/js/polarization.js
@@ -30,11 +30,15 @@ var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAni
 var cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
 var reqId; // Cancels an animation frame request previously scheduled through a call to window.requestAnimationFrame().
 // Normal variables
-var nPoints = 200;
+var nPoints = 500;
 var z = linspace(ndarray([], [nPoints]), 0, 8 * Math.PI);
 var x = ndarray(new Float64Array(nPoints));
 var y = ndarray(new Float64Array(nPoints));
-var speed = 1; // Wave speed
+var auxX = ndarray(new Float64Array(nPoints)); // Auxiliary ndarray
+var auxY = ndarray(new Float64Array(nPoints)); // Auxiliary ndarray
+var k = 1; // Wave number
+var w = 1; // Angular frequency
+var speed = 0.05; // Evolution speed in z direction
 
 
 // Interactive interfaces
@@ -106,19 +110,23 @@ function updateX() {
     /*
      x values will change if time changes.
      x = sin(k * z - w * t)
+     For simplicity, k = 1, w = 1. So period T = 2 * pi.
      */
-    ops.subs(x, z, time); // x = z - t
-    ops.sineq(x); // x = sin(z - t)
+    ops.muls(auxX, z, k); // aux = k * z
+    ops.subs(x, auxX, w * time); // You cannot use ops.subs(x, ops.mulseq(z, k), w * time), it will cause problem.
+    ops.sineq(x); // x = sin(k * z - w * t), by default the amplitude of x is 1.
 }
 
 function updateY() {
     /*
      y values will change if phase or time change.
      y = sin(k * z - w * t + phi)
+     For simplicity, k = 1, w = 1. So period T = 2 * pi.
      */
-    ops.adds(y, z, phi - time); // y = z - t + phi
-    ops.sineq(y); // y = sin(z - t + phi)
-    ops.mulseq(y, BoverA);
+    ops.muls(auxY, z, k); // aux = k * z
+    ops.adds(y, auxY, phi - w * time); // You cannot use ops.subs(y, ops.mulseq(z, k), phi - w * time), it will cause problem.
+    ops.sineq(y); // y = sin(k * z - w * t + phi)
+    ops.mulseq(y, BoverA); // y = y * B / A, adjust y amplitude
 }
 
 function updateZ() {
@@ -126,7 +134,7 @@ function updateZ() {
      z values will change if time changes.
      */
     z = linspace(ndarray([], [nPoints]),
-        time * speed, 10 * Math.PI + time * speed);
+        time * speed, 10 * Math.PI + time * speed); // There are 10/2=5 periods in z direction.
 }
 
 
@@ -229,11 +237,12 @@ function plot() {
 function compute() {
     /*
      Update z every frame and simultaneously update x and y.
+     This is simply a combination of updateX, updateY and updateZ.
      */
     var dt = 0.05;
-    ops.subs(x, z, dt);
+    ops.subs(x, ops.muls(auxX, z, k), w * dt);
     ops.sineq(x);
-    ops.adds(y, z, phi - dt);
+    ops.adds(y, ops.muls(auxY, z, k), phi - w * dt);
     ops.sineq(y);
     ops.mulseq(y, BoverA);
     ops.addseq(z, dt);
