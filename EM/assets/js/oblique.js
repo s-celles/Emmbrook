f71622635bf19c5ca796a06cb94c10bbce4725a0
Sucan <qz2280@columbia.edu>
2017-07-26 22:38:09 -0400

    Working on oblique.js

--- EM/assets/js/oblique.js
+++ EM/assets/js/oblique.js
@@ -10,13 +10,22 @@
 // Import libraries
 var numeric = require('numeric');
 var ndarray = require('ndarray');
-var nj = require('numjs');
+var meshgrid = require('ndarray-meshgrid');
+var ops = require("ndarray-ops");
+var pool = require("ndarray-scratch");
+var nj = require("numjs");
+var unpack = require("ndarray-unpack");
+var cops = require("ndarray-complex"); // Complex arithmetic operations for ndarrays.
+var product = require('tensor-product');
 
 
 // Variables
-var n1Slider = $('#n1').bootstrapSlider();
-var n2Slider = $('#n2').bootstrapSlider();
-var thetaISlider = $('#thetaI').bootstrapSlider();
+var n1Slider = $('#n1')
+    .bootstrapSlider();
+var n2Slider = $('#n2')
+    .bootstrapSlider();
+var thetaISlider = $('#thetaI')
+    .bootstrapSlider();
 var n1 = n1Slider.bootstrapSlider('getValue'); // Get refraction index from slider bar
 var n2 = n2Slider.bootstrapSlider('getValue'); // Get refraction index from slider bar
 var thetaI = thetaISlider.bootstrapSlider('getValue'); // Get incident angle from slider bar
@@ -34,7 +43,8 @@ thetaISlider.on('change', function () {
     updateSlopes();
     plotHeatmap();
 
-    $('#thetaISliderVal').text(thetaI);
+    $('#thetaISliderVal')
+        .text(thetaI);
 });
 
 n1Slider.on('change', function () {
@@ -46,7 +56,8 @@ n1Slider.on('change', function () {
     updateSlopes();
     plotHeatmap();
 
-    $('#n1SliderVal').text(n1);
+    $('#n1SliderVal')
+        .text(n1);
 });
 
 n2Slider.on('change', function () {
@@ -58,7 +69,8 @@ n2Slider.on('change', function () {
     updateSlopes();
     plotHeatmap();
 
-    $('#n2SliderVal').text(n2);
+    $('#n2SliderVal')
+        .text(n2);
 });
 
 
@@ -192,136 +204,174 @@ function createRatioPlot() {
 ///////////////////////////////////////////////////////////////////////////
 //////////////////// EM oblique incidence on media ////////////////////////
 ///////////////////////////////////////////////////////////////////////////
-var xCoord = numeric.linspace(-10, 10, 250);
-var zUpperCoord = numeric.linspace(10, 0, 125);
-var zLowerCoord = numeric.linspace(0, -10, 125);
-var incidentSlope;
-var reflectSlope;
-var transmitSlope;
+var zNum = 250;
+var xNum = 125;
+var zCoord = numeric.linspace(-10, 10, zNum);
+var xCoord = numeric.linspace(0, 10, xNum);
+var zStep = (zCoord[zCoord.length - 1] - zCoord[0]) / zNum;
+var xStep = (xCoord[xCoord.length - 1] - xCoord[0]) / xNum;
+var optionIndices = numeric.linspace(0, 2, 3);
+var lambda = 1;
+// Something similar to np.meshgrid, but less straightforward to see, thus
+// you should check the documentation detaily on
+// http://scijs.net/packages/#scijs/ndarray.
+var mesh = meshgrid([-10, 10, zStep], [0, 10, xStep], [0, 2, 1]);
+var zMesh = mesh.pick(null, 0); // Pick the first column of mesh
+var xMesh = mesh.pick(null, 1); // Pick the second column of mesh
+var iMesh = mesh.pick(null, 2); // Pick the third column of mesh
+zMesh = ndarray(zMesh, [zNum + 1, xNum + 1, 3]); // Reshape zMesh to (zNum + 1, xNum + 1, 3)
+xMesh = ndarray(xMesh, [zNum + 1, xNum + 1, 3]); // Reshape xMesh to (xNum + 1, xNum + 1, 3)
+iMesh = ndarray(iMesh, [zNum + 1, xNum + 1, 3]); // Reshape iMesh to (zNum + 1, xNum + 1, 3)
 
 
-function kr(kx, ky, kz, x, y, z) {
+function generateKVector(params) {
     /*
-     Wave vector times position vector. Input Cartesian coordinates xx, yy, and zz,
-     then function will return scalar value of $\mathbf{k} \cdot \mathbf{r}$, where
-     $\mathbf{k} = (kx, ky, kz)$, $\mathbf{r} = (x, y, z)$.
+     kVector = k0 * [n1, n1, n2]
      */
-    var metricTensor = [
-        [1, 0, 0],
-        [0, 0, 0],
-        [0, 0, 1]
-    ];
-    return numeric.dot([kx, ky, kz], numeric.dot(metricTensor, [x, y, z]));
+    var k0 = 2 * Math.PI / lambda; // Free-space wavenumber
+    var kVector = ndarray(new Float64Array(3)); // Wavenumbers for incident, reflected, transmitted waves
+    ops.muls(kVector, ndarray(new Float64Array([n1, n1, n2])), k0);
+    return kVector;
 }
 
-function updateUpperAmplitude() {
+function generateKxAndKz() {
     /*
-     The incident E-field amplitude changes with x, y, z spatial coordinates, and thetaI.
+     Calculate z component for incident, reflected, and transmitted wave's k vector,
+     and x component for incident, reflected, and transmitted wave's k vector.
      */
-    var amp = [];
-    var r = updateRatioValues(thetaI)[0];
-    for (var i = 0; i < xCoord.length; i++) {
-        amp[i] = [];
-        for (var j = 0; j < zUpperCoord.length; j++) {
-            var normalizeK = 1 / Math.sqrt(1 + Math.pow(-1 / incidentSlope, 2));
-            var normailizeKp = 1 / Math.sqrt(1 + Math.pow(-1 / reflectSlope, 2));
-            var kDotR = kr(normalizeK, 0, -normalizeK / incidentSlope, xCoord[i], 0, zUpperCoord[j]);
-            var kpDotR = kr(normailizeKp, 0, -normailizeKp / reflectSlope, xCoord[i], 0, zUpperCoord[j]);
-            amp[i][j] = Math.sqrt(
-                Math.pow(Math.cos(thetaI) * Math.cos(kDotR) +
-                    r * Math.cos(thetaI) * Math.cos(kpDotR), 2) +
-                Math.pow(-Math.sin(thetaI) * Math.cos(kDotR) +
-                    r * Math.sin(thetaI) * Math.cos(kpDotR), 2)
-            )
-        }
-    }
-
-    return amp;
+    var kVector = generateKVector();
+    var thetaT = Math.asin(n1 / n2 * Math.sin(thetaI)); // Transmitted angle from Snell's law
+    var kZ = ndarray(new Float64Array(3)); // Incident, reflected, transmit wave's kz
+    var kX = ndarray(new Float64Array(3)); // Incident, reflected, transmit wave's kx
+    ops.mul(kZ, ndarray([Math.cos(thetaI), -Math.cos(thetaI), Math.cos(thetaT)]), kVector); // Element-wise multiply
+    ops.mul(kX, ndarray([Math.sin(thetaI), Math.sin(thetaI), Math.sin(thetaT)]), kVector); // Element-wise multiply
+    return [kZ, kX];
 }
 
-function updateTransmitAmplitude() {
+function generateMask() {
     /*
-     The transmissive E-field amplitude changes with x, y, z spatial coordinates, n1, n2 and thetaI.
+     Masks regions: z<0 for I, R; z>0 for T
      */
-    var transamp = [];
-    for (var i = 0; i < xCoord.length; i++) {
-        transamp[i] = [];
-        for (var j = 0; j < zLowerCoord.length; j++) {
-            transamp[i][j] = updateRatioValues(thetaI)[1];
-        }
-    }
+    // 1 for positive z, 0 otherwise
+    var greaterEqualThan0 = pool.malloc(iMesh.shape);
+    ops.geqs(greaterEqualThan0, zMesh, 0); // greaterEqualThan0 is a true/false grid.
+    ops.mulseq(greaterEqualThan0, 1); // You need to numerify it.
 
-    return transamp;
+    // 1 for negative z, 0 otherwise
+    var lessThan0 = pool.malloc(iMesh.shape);
+    ops.lts(lessThan0, zMesh, 0); // lessThan0 is a true/false grid.
+    ops.mulseq(lessThan0, 1); // You need to numerify it.
+
+    return [greaterEqualThan0, lessThan0];
 }
 
-function updateAmplitudes() {
-    return nj.concatenate(updateUpperAmplitude(), updateTransmitAmplitude()).transpose().tolist();
+function generateGeneralAmplitude() {
+    /* 
+     Consider amplitudes for the three waves together.
+     */
+    var r, t;
+    [r, t] = updateRatioValues(thetaI); // Reflected and transmitted amplitudes
+    var A = pool.malloc(iMesh.shape);
+    var aux = pool.malloc(iMesh.shape); // Auxiliary grid
+    ops.eqs(aux, iMesh, 0); // aux[i,j,k] = true if iMesh[i,j,k] === 0
+    ops.addeq(A, ops.mulseq(aux, 1)); // A += np.equal(iMesh, 0) * 1
+    ops.eqs(aux, iMesh, 1); // aux[i,j,k] = true if iMesh[i,j,k] === 1
+    ops.addeq(A, ops.mulseq(aux, r)); // A += np.equal(iMesh, 1) * r
+    ops.eqs(aux, iMesh, 2); // aux[i,j,k] = true if iMesh[i,j,k] === 2
+    ops.addeq(A, ops.mulseq(aux, t)); // A += np.equal(iMesh, 2) * t
+
+    var greaterEqualThan0, lessThan0;
+    [greaterEqualThan0, lessThan0] = generateMask();
+    ops.muleq(A.pick(null, null, 0), lessThan0.pick(null, null, 0)); // A[:, :, 0] *= lessThan0[:, :, 0]
+    ops.muleq(A.pick(null, null, 1), lessThan0.pick(null, null, 1)); // A[:, :, 1] *= lessThan0[:, :, 1]
+    ops.muleq(A.pick(null, null, 2), greaterEqualThan0.pick(null, null, 2)); // A[:, :, 0] *= greaterEqualThan0[:, :, 0]
+    return A;
 }
 
-function updateSlopes() {
+function multiplication(a, b) {
+    return a * b
+}
+
+function tensorContraction(mesh, vec) {
     /*
-     Update incident/reflective/transmitted light slopes according to slider motion.
-     Those change when thetaI, n1, and n2 change.
+     This defines a tensor contraction.
      */
-    var thetaT = Math.asin(n1 / n2 * Math.sin(thetaI)); // Transmission angle
-    incidentSlope = -Math.tan(thetaI);
-    reflectSlope = Math.tan(thetaI);
-    transmitSlope = -Math.tan(thetaT);
+    return product.bind(multiplication, mesh.shape, vec.shape);
 }
 
-
-// Plot
-function plotHeatmap() {
-    plt0.data[0].z = updateAmplitudes();
-
-    Plotly.redraw(plt0);
+function generateEachAmplitude() {
+    /*
+     Generate individual wave amplitudes for incident, reflected, and transmitted.
+     */
+    var time = 0;
+    var kZ, kX;
+    [kZ, kX] = generateKxAndKz();
+    console.log(zMesh.shape, kZ.shape)
+    var A = generateGeneralAmplitude();
+    var kzz = tensorContraction(zMesh, kZ);
+    console.log(kzz)
 }
 
-function createHeatmap() {
-    var trace = {
-        x: xCoord,
-        y: zUpperCoord.concat(zLowerCoord),
-        z: updateAmplitudes(),
-        type: 'heatmap',
-        zmin: 0,
-        zmax: 2
-    };
+generateEachAmplitude();
 
-    var texts = {
-        x: [-8, -8],
-        y: [5, -5],
-        text: ['medium 1', 'medium 2'],
-        mode: 'text',
-        textfont: {
-            color: '#ffffff'
-        }
-    };
 
-    var data = [trace, texts];
 
-    var layout = {
-        title: 'E filed amplitude',
-        yaxis: {
-            title: 'z'
-        },
-        xaxis: {
-            title: 'x'
-        }
-    };
 
-    Plotly.newPlot('plt0', data, layout);
-}
+
+// // Plot
+// function plotHeatmap() {
+//     plt0.data[0].z = updateAmplitudes();
+
+//     Plotly.redraw(plt0);
+// }
+
+// function createHeatmap() {
+//     var trace = {
+//         x: xCoord,
+//         y: zUpperCoord.concat(zLowerCoord),
+//         z: updateAmplitudes(),
+//         type: 'heatmap',
+//         zmin: 0,
+//         zmax: 2
+//     };
+
+//     var texts = {
+//         x: [-8, -8],
+//         y: [5, -5],
+//         text: ['medium 1', 'medium 2'],
+//         mode: 'text',
+//         textfont: {
+//             color: '#ffffff'
+//         }
+//     };
+
+//     var data = [trace, texts];
+
+//     var layout = {
+//         title: 'E filed amplitude',
+//         yaxis: {
+//             title: 'z'
+//         },
+//         xaxis: {
+//             title: 'x'
+//         }
+//     };
+
+//     Plotly.newPlot('plt0', data, layout);
+// }
+
 
 
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////// Initialize //////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
-$('#thetaISliderVal').text(thetaI);
-$('#n1SliderVal').text(n1);
-$('#n2SliderVal').text(n2);
+$('#thetaISliderVal')
+    .text(thetaI);
+$('#n1SliderVal')
+    .text(n1);
+$('#n2SliderVal')
+    .text(n2);
 // Left panel
-updateSlopes();
-createHeatmap();
 // Right panel
 [reflectRatioList, transmitRatioList] = updateRatioLists();
 createRatioPlot();
